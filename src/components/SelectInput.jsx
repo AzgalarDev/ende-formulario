d9196fccc791a9c440e4502bcd893ba036c1379a
Azgalar <sergiolara88@hotmail.com>
2022-11-09 00:52:26 -0400

    first commit

---
+++ src/components/SelectInput.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Formik, Form, useField } from 'formik';
+
+  const SelectInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+export default SelectInput
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import FormRegister from './pages/FormRegister'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FormRegister/>
+  </React.StrictMode>
+)
